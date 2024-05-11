935f365ab79d40ffd3d3ff06258b2bc7e62ce538
spectrum72 <spectrummentle@gmail.com>
2024-05-12 01:10:08 +0900

    hello world

--- discordbot.py
+++ discordbot.py
@@ -4,9 +4,10 @@ import discord
 from discord.ext import commands
 from dotenv import load_dotenv
 import os
+import asyncio
 load_dotenv()
 
-PREFIX = os.environ['PREFIX']
+#REFIX = os.environ['PREFIX']
 TOKEN = os.environ['TOKEN']
 
 client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
@@ -14,6 +15,11 @@ client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 @client.event
 async def on_ready():
     print(f'Logged in as {client.user}.')
+
+async def load():
+    for file in os.listdir("./Cogs"):
+        if file.endswith(".py"):
+            await client.load_extension(f".Cogs/{file[:-3]}")
 
 @client.event
 async def on_message(message):
@@ -23,11 +29,11 @@ async def on_message(message):
     if message.content == "으응이 손":
         await message.channel.send("손!")
 
-    if message.content == f'{PREFIX}call':
-        await message.channel.send("callback!")
+    #if message.content == f'{PREFIX}call':
+    #    await message.channel.send("callback!")
 
-    if message.content.startswith(f'{PREFIX}hello'):
-        await message.channel.send('Hello!')
+    #if message.content.startswith(f'{PREFIX}hello'):
+    #    await message.channel.send('Hello!')
 
 
 try:
