a3757bc799a42df6ce692fb47bc34336f9076f35
spectrum72 <spectrummentle@gmail.com>
2024-05-14 20:43:58 +0900

    hello world

--- discordbot.py
+++ discordbot.py
@@ -6,7 +6,9 @@ from dotenv import load_dotenv
 from PyKakao import KoGPT
 import os
 import asyncio
+
 load_dotenv()
+api = KoGPT(service_key = '8597e85508f4673ad4b63a8fa540a834')
 
 #REFIX = os.environ['PREFIX']
 CLIENT_TOKEN = os.environ['CLIENT_TOKEN']
@@ -16,7 +18,6 @@ client = commands.Bot(command_prefix="/", intents=discord.Intents.all())
 @client.event
 async def on_ready():
     
-    api = KoGPT(service_key = '8597e85508f4673ad4b63a8fa540a834')
     print(f'Logged in as {client.user}.')
     
 async def load():
@@ -31,6 +32,16 @@ async def on_message(message):
     
     if message.content == "으응이 손":
         await message.channel.send("손!")
+        
+    if message.content == "hello world mf":
+        # 필수 파라미터
+        prompt = "인간처럼 생각하고, 행동하는 '지능'을 통해 인류가 이제까지 풀지 못했던"
+        max_tokens = 64
+
+        # 결과 조회
+        result = api.generate(prompt, max_tokens, temperature=0.7, top_p=0.8)
+        
+        await message.channel.send(result[0].text)
 
     #if message.content == f'{PREFIX}call':
     #    await message.channel.send("callback!")
